dc2758e2f877c7eaf9c300ee525bbcad3d55fe4e
Leomaris Reyes <reyes.leomaris@gmail.com>
2018-08-01 23:51:16 -0400

    AdvancedPopUpSample AskXammy Blog.

---
+++ AdvancedPopUpSample/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Animations;
+using Rg.Plugins.Popup.Enums;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;  
+
+namespace AdvancedPopUpSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var pr = new PopUp(); 
+             var scaleAnimation = new ScaleAnimation
+            {
+                PositionIn = MoveAnimationOptions.Right,
+                PositionOut = MoveAnimationOptions.Left 
+             };
+
+            pr.Animation = scaleAnimation; 
+            await PopupNavigation.PushAsync(pr);
+        }
+
+    }
+}
---
+++ AdvancedPopUpSample/PopUp.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace AdvancedPopUpSample
+{
+    public partial class PopUp //: PopupPage
+    {
+        public PopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
